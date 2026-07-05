063e5aa7bd221e672a1c97b87586a8a72899e21f
Apostolos Tsano <tolitsano18@gmail.com>
2026-07-05 01:27:52 -0400

    added vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './app.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import './styles.css'
+import { inject } from '@vercel/analytics';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
